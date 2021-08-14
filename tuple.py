94ef5814b6938b6d759be4c975a9fd10faf491ef
Shubham-6776 <88704345+Shubham-6776@users.noreply.github.com>
2021-08-14 14:56:06 +0530

    hype

---
+++ tuple.py
@@ -0,0 +1,8 @@
+x=["happy",456,789,"ice",98,73.2,65,"picture",34,87]
+print("\n list x : ",x[:])
+print("\n list x[2:5] : ",x[2:5])
+print("\n list negative indexing x[::-1] : ",x[::-1])
+z =x.count("happy")
+print(z,"\nnumber of happy string found in tuple")
+z=x.index(789)
+print("\nindex number of 789 is :",z)

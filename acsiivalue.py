a448cd9be007111cac2a7106c34cb3f261d47a48
Shubham-6776 <88704345+Shubham-6776@users.noreply.github.com>
2021-08-16 13:43:51 +0530

    hype

---
+++ acsiivalue.py
@@ -0,0 +1,4 @@
+ch = input("Enter letter X: ")
+print("\nThe ASCII value of char " + ch + " is: ",ord(ch))
+sn = input("Enter letter x: ")
+print("\nThe ASCII value of char " + sn + " is: ",ord(sn))
